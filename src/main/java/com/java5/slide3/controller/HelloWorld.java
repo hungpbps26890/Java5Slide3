1435d22592e22dc8ba402a7058a0999b5df7dc86
hungpbps26890 <115541488+hungpbps26890@users.noreply.github.com>
2024-01-18 15:50:31 +0700

    Update HelloWorld.java - This code is from GitHub

--- src/main/java/com/java5/slide3/controller/HelloWorld.java
+++ src/main/java/com/java5/slide3/controller/HelloWorld.java
@@ -3,5 +3,6 @@ package com.java5.slide3.controller;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		System.out.println("This code is from GitHub");
 	}
 }
